6bdf24c040aa08aac1099669aa5328eda1241861
Suhrud Valaparla <suhrud.valaparla@peopletech.com>
2026-04-06 18:57:34 +0530

    ppl7152555

--- terraform/nsg.tf
+++ terraform/nsg.tf
@@ -1,47 +1,79 @@
-resource "azurerm_network_security_group" "nsg" {
-  name                = "nsg-3tier"
-  location            = data.azurerm_resource_group.rg.location
-  resource_group_name = data.azurerm_resource_group.rg.name
+# Resource Group reference
+resource "azurerm_resource_group" "rg" {
+  name     = "TrainingandPoC"
+  location = "East US"
 }
 
-# SSH
+# Virtual Network
+resource "azurerm_virtual_network" "vnet" {
+  name                = "vnet-3tier"
+  address_space       = ["10.0.0.0/16"]
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+}
+
+# Subnet
+resource "azurerm_subnet" "subnet" {
+  name                 = "subnet-3tier"
+  resource_group_name  = azurerm_resource_group.rg.name
+  virtual_network_name = azurerm_virtual_network.vnet.name
+  address_prefixes     = ["10.0.1.0/24"]
+}
+
+# Network Security Group
+resource "azurerm_network_security_group" "nsg" {
+  name                = "nsg-3tier"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+}
+
+# SSH rule
 resource "azurerm_network_security_rule" "ssh" {
   name                        = "ssh"
   priority                    = 100
   direction                   = "Inbound"
   access                      = "Allow"
   protocol                    = "Tcp"
-  destination_port_range      = "22"
-  source_address_prefix       = "*"
-  destination_address_prefix  = "*"
-  resource_group_name         = data.azurerm_resource_group.rg.name
-  network_security_group_name = azurerm_network_security_group.nsg.name
+  source_port_range            = "*"            # required
+  destination_port_range       = "22"           # required
+  source_address_prefix        = "*"            # allow any IP
+  destination_address_prefix   = "*"            # all VM IPs
+  network_security_group_name  = azurerm_network_security_group.nsg.name
+  resource_group_name          = azurerm_resource_group.rg.name
 }
 
-# Frontend
-resource "azurerm_network_security_rule" "frontend" {
-  name                        = "frontend"
+# HTTP rule (frontend)
+resource "azurerm_network_security_rule" "http" {
+  name                        = "http"
   priority                    = 200
   direction                   = "Inbound"
   access                      = "Allow"
   protocol                    = "Tcp"
-  destination_port_range      = "3000"
-  source_address_prefix       = "*"
-  destination_address_prefix  = "*"
-  resource_group_name         = data.azurerm_resource_group.rg.name
-  network_security_group_name = azurerm_network_security_group.nsg.name
+  source_port_range            = "*"
+  destination_port_range       = "80"
+  source_address_prefix        = "*"
+  destination_address_prefix   = "*"
+  network_security_group_name  = azurerm_network_security_group.nsg.name
+  resource_group_name          = azurerm_resource_group.rg.name
 }
 
-# Backend
+# Custom port for backend (example 5000)
 resource "azurerm_network_security_rule" "backend" {
   name                        = "backend"
   priority                    = 300
   direction                   = "Inbound"
   access                      = "Allow"
   protocol                    = "Tcp"
-  destination_port_range      = "5000"
-  source_address_prefix       = "*"
-  destination_address_prefix  = "*"
-  resource_group_name         = data.azurerm_resource_group.rg.name
-  network_security_group_name = azurerm_network_security_group.nsg.name
+  source_port_range            = "*"
+  destination_port_range       = "5000"
+  source_address_prefix        = "*"
+  destination_address_prefix   = "*"
+  network_security_group_name  = azurerm_network_security_group.nsg.name
+  resource_group_name          = azurerm_resource_group.rg.name
+}
+
+# Associate NSG with Subnet
+resource "azurerm_subnet_network_security_group_association" "subnet_nsg_assoc" {
+  subnet_id                 = azurerm_subnet.subnet.id
+  network_security_group_id = azurerm_network_security_group.nsg.id
 }
