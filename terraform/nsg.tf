6fa3a0d912342449e0bdff3764329841a469df06
Suhrud Valaparla <suhrud.valaparla@peopletech.com>
2026-04-07 10:16:07 +0530

    today

--- terraform/nsg.tf
+++ terraform/nsg.tf
@@ -1,24 +1,4 @@
-# Resource Group reference
-resource "azurerm_resource_group" "rg" {
-  name     = "TrainingandPoC"
-  location = "East US"
-}
 
-# Virtual Network
-resource "azurerm_virtual_network" "vnet" {
-  name                = "vnet-3tier"
-  address_space       = ["10.0.0.0/16"]
-  location            = azurerm_resource_group.rg.location
-  resource_group_name = azurerm_resource_group.rg.name
-}
-
-# Subnet
-resource "azurerm_subnet" "subnet" {
-  name                 = "subnet-3tier"
-  resource_group_name  = azurerm_resource_group.rg.name
-  virtual_network_name = azurerm_virtual_network.vnet.name
-  address_prefixes     = ["10.0.1.0/24"]
-}
 
 # Network Security Group
 resource "azurerm_network_security_group" "nsg" {
